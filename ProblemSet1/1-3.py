f5c38a3891fd651df4b077380f482d87e87a8fc6
Yueming Sun <cynthia0828@gmail.com>
2015-06-19 15:58:02 -0400

    Update 1-3.py

--- ProblemSet1/1-3.py
+++ ProblemSet1/1-3.py
@@ -1,3 +1,4 @@
+# Solution 1:
 def find(s):
     # return longest substring by printing this string
     # from the start to the end index using s[start:start + maxLen]
@@ -21,3 +22,44 @@ def find(s):
 
 print find('abcabcdopaqrstr')
     
+# Solution 2:
+ # get the largest number in a list
+def maxNumInList(L):
+    maxNum = L[0]
+    for i in L:
+        if i > maxNum:
+            maxNum = i
+    return maxNum
+    
+##L = [1,2,3,9,5,6,0]
+##maxNumInList(L)
+
+# get the length of each sequenced string with increasing order
+# store it in a list
+def maxNum(s):
+    count = 1
+    numlist = []
+    point = [0]
+    for i in range(1, len(s)):
+        if s[i-1] <= s[i]:
+            count += 1
+        else:
+            point.append(i)
+            numlist.append(count)
+            count = 1
+    numlist.append(count)
+    
+    longestLen = maxNumInList(numlist) # the length of the longest string
+    position = numlist.index(longestLen) # the position of the longest string in the max number list
+    index = point[position] # the start index in the actual string
+
+    ss = ''
+    for i in range(index, index + longestLen): # print out the first longest string
+        ss += s[i]
+
+    print 'Longest substring in alphabetical order is: ' + ss
+
+# s = 'a'
+maxNum(s)
+
+##print maxNumInList(maxNum(s))
