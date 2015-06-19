8a410b9baab8bf9073e0ff8cbfa45eb8a6866fbe
Yueming Sun <cynthia0828@gmail.com>
2015-06-19 18:17:05 -0400

    Update 2-1 Paying the Minimum.py

--- ProblemSet2/2-1 Paying the Minimum.py
+++ ProblemSet2/2-1 Paying the Minimum.py
@@ -1,5 +1,15 @@
 # https://courses.edx.org/courses/course-v1:MITx+6.00.1x_6+2T2015/courseware/Week_2/Problem_Set_2/
-# Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
+# Write a program to calculate the credit card balance after one year 
+# if a person only pays the minimum monthly payment required by the credit card company each month.
+
+# The following variables contain values as described below:
+#
+#    balance - the outstanding balance on the credit card
+#
+#    annualInterestRate - annual interest rate as a decimal
+#
+#    monthlyPaymentRate - minimum monthly payment rate as a decimal
+
 
 def payFun(balance,annualInterestRate,monthlyPaymentRate):
     
