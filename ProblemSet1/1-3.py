bdcc6b55c13f1d9e777334efabf7b71b2fc655e6
Yueming Sun <cynthia0828@gmail.com>
2015-06-19 16:01:36 -0400

    Update 1-3.py

--- ProblemSet1/1-3.py
+++ ProblemSet1/1-3.py
@@ -1,6 +1,6 @@
 # Alphabetical Substrings
-# Assume s is a string of lower case characters.
 
+# Assume s is a string of lower case characters.
 # Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if s = 'azcbobobegghakl', then your program should print
 # Longest substring in alphabetical order is: beggh
 
