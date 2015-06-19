9c0c726ad9730e7c4fc3f1f15bbdcf36f178fb27
Yueming Sun <cynthia0828@gmail.com>
2015-06-19 16:06:01 -0400

    Update 1-2 Counting Bobs.py

--- ProblemSet1/1-2 Counting Bobs.py
+++ ProblemSet1/1-2 Counting Bobs.py
@@ -2,7 +2,8 @@
 ##
 ##Assume s is a string of lower case characters.
 ##
-##Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your program should print
+##Write a program that prints the number of times the string 'bob' occurs in s. For example, 
+if s = 'azcbobobegghakl', then your program should print
 ##
 ##Number of times bob occurs is: 2
 
