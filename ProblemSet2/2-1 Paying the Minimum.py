b6bcdbc72788bf8c1f1cbdd02db6aba258e42d76
Yueming Sun <cynthia0828@gmail.com>
2015-06-20 11:21:12 -0400

    Update 2-1 Paying the Minimum.py

--- ProblemSet2/2-1 Paying the Minimum.py
+++ ProblemSet2/2-1 Paying the Minimum.py
@@ -16,25 +16,12 @@ def payFun(balance,annualInterestRate,monthlyPaymentRate):
     monthlyInterestRate = annualInterestRate/12.0
     totalPay = 0
     
-    for i in range(1,2):
-
-        minMonPay = balance * monthlyPaymentRate
-        totalPay = minMonPay
-        balance = balance - minMonPay
-        balance = balance * (1 + monthlyInterestRate)
+    for i in range(1,13):
         
-        print 'Month: ' + str(i) 
-        print 'Minimum monthly payment: ' +  '%.2f' % minMonPay 
-        print 'Remaining balance: ' + '%.2f' % balance
-
-    for i in range(2,13):
-        
-        minMonPay = balance * monthlyPaymentRate #2
+        minMonPay = balance * monthlyPaymentRate 
         totalPay += minMonPay
-        balance = balance - minMonPay #3
+        balance = balance - minMonPay 
         balance = balance * (1 + monthlyInterestRate)
-        
-##        balance = balance - balance * monthlyPaymentRate 
 
         print 'Month: ' + str(i)
         print 'Minimum monthly payment: ' +  '%.2f' % minMonPay 
@@ -46,4 +33,4 @@ def payFun(balance,annualInterestRate,monthlyPaymentRate):
 ##print payFun(5000,0.18,0.02)
 ##print payFun(4842,0.2,0.04)
 
-payFun(balance,annualInterestRate,monthlyPaymentRate)
+##payFun(balance,annualInterestRate,monthlyPaymentRate)
