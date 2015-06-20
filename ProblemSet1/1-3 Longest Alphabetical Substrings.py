a08c4f5d80a4f21feeb21cd9ed311742d5128d66
Yueming Sun <cynthia0828@gmail.com>
2015-06-20 00:04:04 -0400

    Update 1-3 Longest Alphabetical Substrings.py

--- ProblemSet1/1-3 Longest Alphabetical Substrings.py
+++ ProblemSet1/1-3 Longest Alphabetical Substrings.py
@@ -1,7 +1,8 @@
 # Alphabetical Substrings
 
 # Assume s is a string of lower case characters.
-# Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if s = 'azcbobobegghakl', then your program should print
+# Write a program that prints the longest substring of s in which the letters occur in alphabetical order. 
+# For example, if s = 'azcbobobegghakl', then your program should print
 # Longest substring in alphabetical order is: beggh
 
 # In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print
