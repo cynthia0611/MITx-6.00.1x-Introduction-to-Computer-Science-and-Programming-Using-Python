b189bd7f3737d73f350d0f357f04489390b64e21
Yueming Sun <cynthia0828@gmail.com>
2015-06-23 17:43:43 -0400

    Create lectureCode_Lec5_Fibonacci.py

---
+++ Lecture5/lectureCode_Lec5_Fibonacci.py
@@ -0,0 +1,24 @@
+
+def fib(x):
+    """assumes x an int >= 0
+       returns Fibonacci of x"""
+    assert type(x) == int and x >= 0
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fib(x-1) + fib(x-2)
+    
+##print fib(0)
+##print fib(1)
+##print fib(2)
+##print fib(3)
+##print fib(4)
+##print fib(5)
+##print fib(6)
+##1
+##1
+##2
+##3
+##5
+##8
+##13
