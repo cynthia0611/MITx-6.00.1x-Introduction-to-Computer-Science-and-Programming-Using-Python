706da1c76035c76c87fdf8afae4daa2513ee7a77
Yueming Sun <cynthia0828@gmail.com>
2015-06-27 15:50:02 -0400

    Update 3-3 getGuessedWord.py

--- ProblemSet3/3-3 getGuessedWord.py
+++ ProblemSet3/3-3 getGuessedWord.py
@@ -3,7 +3,12 @@
 # based on what letters in lettersGuessed are in secretWord. 
 
 def getGuessedWord(secretWord, lettersGuessed):
-
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
     ans = ''
     for letter in secretWord:
         if letter in lettersGuessed:
