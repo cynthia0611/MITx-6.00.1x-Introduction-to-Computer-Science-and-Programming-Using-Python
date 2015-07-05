a3a43250432d31a8245cfd3ea5f0e5c100c9766e
Yueming Sun <cynthia0828@gmail.com>
2015-07-05 00:08:19 -0400

    Create L7 PROBLEM 4 glass testing.py

---
+++ Lecture7/L7 PROBLEM 4 glass testing.py
@@ -0,0 +1,17 @@
+def union(set1, set2):
+   """
+   set1 and set2 are collections of objects, each of which might be empty.
+   Each set has no duplicates within itself, but there may be objects that
+   are in both sets. Objects are assumed to be of the same type.
+
+   This function returns one set containing all elements from
+   both input sets, but with no duplicates.
+   """
+   if len(set1) == 0:
+      return set2
+   elif set1[0] in set2:
+      return union(set1[1:], set2)
+   else:
+      return set1[0] + union(set1[1:], set2)
+      
+# Test Suite : union('','abc'), union('a','abc'), union('ab','abc'), union('d','abc')
