90bd85fb5f3520f154ae54415a0d561116b1b06d
Yueming Sun <cynthia0828@gmail.com>
2015-07-05 00:04:49 -0400

    Update L7 PROBLEM 2 blackBox Testing.py

--- Lecture7/L7 PROBLEM 2 blackBox Testing.py
+++ Lecture7/L7 PROBLEM 2 blackBox Testing.py
@@ -8,9 +8,9 @@ def union(set1, set2):
    both input sets, but with no duplicates.
    """
 
-Test cases:   
- set1 is an empty set; 
- set2 is an empty set  set1 is an empty set; 
- set2 is of size greater than or equal to 1  set1 is of size greater than or equal to 1; 
- set2 is an empty set  set1 and set2 are both nonempty sets which do not contain any objects in common  
- set1 and set2 are both nonempty sets which contain objects in common
+# Test cases:   
+ # set1 is an empty set; 
+ # set2 is an empty set  set1 is an empty set; 
+ # set2 is of size greater than or equal to 1  set1 is of size greater than or equal to 1; 
+ # set2 is an empty set  set1 and set2 are both nonempty sets which do not contain any objects in common  
+ # set1 and set2 are both nonempty sets which contain objects in common
