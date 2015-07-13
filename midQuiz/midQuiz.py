2dbf0ef6af7718ec32426cb38c3a2d0c0de7eb40
Yueming Sun <cynthia0828@gmail.com>
2015-07-13 12:15:41 -0400

    Update midQuiz.py

--- midQuiz/midQuiz.py
+++ midQuiz/midQuiz.py
@@ -221,4 +221,33 @@
 ##    
 ##print sumDigits(1234)
 
-    
+##Problem 7 (10/10 points)
+
+##Write a Python function that returns a list of keys in aDict with the value target. 
+##The list of keys you return should be sorted in increasing order. 
+##The keys and values in aDict are both integers. (If aDict does not contain the value target, 
+##you should return an empty list.)
+
+##This function takes in a dictionary and an integer and returns a list.
+def keysWithValue(aDict, target):
+    '''
+    aDict: a dictionary
+    target: an integer
+    '''
+    # Your code here 
+    values = aDict.values()
+    keysList = []
+    if target in values:
+        for i in aDict.keys():
+
+            if aDict[i] == target:
+                keysList.append(i)
+
+                keysList.sort()
+
+        return keysList
+    else:
+        return []
+print keysWithValue({56:23,2:32,20:34,4:59,5:23,6:23,7:23},23)
+
+
