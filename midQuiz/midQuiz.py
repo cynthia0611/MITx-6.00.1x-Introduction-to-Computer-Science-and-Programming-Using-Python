9cf48635340e4ffc67d1087564e0258f7ff5e918
Yueming Sun <cynthia0828@gmail.com>
2015-07-13 17:12:32 -0400

    Update midQuiz.py

--- midQuiz/midQuiz.py
+++ midQuiz/midQuiz.py
@@ -251,3 +251,91 @@ def keysWithValue(aDict, target):
 print keysWithValue({56:23,2:32,20:34,4:59,5:23,6:23,7:23},23)
 
 
+##Problem 8 (20/20 points)
+
+##Write a Python function called satisfiesF that has the specification below. 
+##Then make the function call run_satisfiesF(L, satisfiesF). Your code should look like:
+
+def satisfiesF(L):
+    """
+    Assumes L is a list of strings
+    Assume function f is already defined for you and it maps a string to a Boolean
+    Mutates L such that it contains all of the strings, s, originally in L such
+            that f(s) returns True, and no other elements
+    Returns the length of L after mutation
+    """
+    # Your function implementation here
+
+run_satisfiesF(L, satisfiesF)
+
+For your own testing of satisfiesF, for example, the following test function f and test code:
+
+def f(s):
+    return 'a' in s
+      
+L = ['a', 'b', 'a']
+print satisfiesF(L)
+print L
+
+Should print:
+
+2
+['a', 'a']
+
+Paste your entire function satisfiesF, including the definition, 
+in the box below. After you define your function, make a function call to run_satisfiesF(L, satisfiesF). 
+Do not define f or run_satisfiesF. Do not leave any debugging print statements.
+
+For this question, you will not be able to see the test cases we run. 
+This problem will test your ability to come up with your own test cases.
+
+Solution 1:
+def f(s):
+    return 'a' in s
+
+def satisfiesF(L):
+    """
+    Assumes L is a list of strings
+    Assume function f is already defined for you and it maps a string to a Boolean
+    Mutates L such that it contains all of the strings, s, originally in L such
+            that f(s) returns True, and no other elements
+    Returns the length of L after mutation
+    """
+    # Your function implementation here
+
+    newL = []
+    for i in L:
+
+        if f(i) == True:
+            newL.append(i)
+
+            #shouldn't modify a list while iterate over it.
+    L[:] = newL	
+
+    return len(L)
+
+#L = ['a', 'b', 'a','a','c','a','p','w']
+#L = ['a', 'b', 'a']
+
+print satisfiesF(L)
+print L
+
+Solution 2:
+    
+def satisfiesF(L):
+
+   count= len(L)
+   rec_list=list()
+
+   for i in range(0,count):
+     if(f(L[i])==False):
+        rec_list.append(L[i])   
+
+   if(rec_list):
+     for j in rec_list:
+      L.remove(j)
+
+
+
+   return len(L)
+run_satisfiesF(L, satisfiesF)
