da1512574093274d935f8cd7a57652c35b24f752
Cynthia Sun <cynthia0828@gmail.com>
2015-07-13 22:15:45 -0400

    Modify mid-term quiz

--- midQuiz/midQuiz.py
+++ midQuiz/midQuiz.py
@@ -157,25 +157,25 @@
 ##b is an integer greater than or equal to 2.
 ##Your function should return an integer answer.
 
-##def myLog(x, b):
-##    '''
-##    x: a positive integer
-##    b: a positive integer; b >= 2
-##
-##    returns: log_b(x), or, the logarithm of x relative to a base b.
-##    '''
-##    
-##    exp = 2
-##    result = 0
-##    if x > b:
-##        while result <= x:
-##            result = b**exp
-##            exp += 1
-##        return exp - 2
-##    else:
-##        return 0
-##
-##print myLog(16,22)
+def myLog(x, b):
+    '''
+    x: a positive integer
+    b: a positive integer; b >= 2
+
+    returns: log_b(x), or, the logarithm of x relative to a base b.
+    '''
+    
+    exp = 2
+    result = 0
+    if x > b:
+        while result <= x:
+            result = b**exp
+            exp += 1
+        return exp - 2
+    else:
+        return 0
+
+print myLog(16,22)
 
 ##PROBLEM 5  (10 points possible)
 ##Write a Python function that returns the sublist of strings in aList
@@ -186,17 +186,17 @@
 ##This function takes in a list of strings and returns a list of strings.
 ##Your function should not modify aList.
 ##
-##def lessThan4(aList):
-##    '''
-##    aList: a list of strings
-##    '''
-##    bList = []
-##    for i in range(len(aList)):
-##        if len(aList[i]) < 4:
-##            bList.append(aList[i])
-##    return bList
+def lessThan4(aList):
+    '''
+    aList: a list of strings
+    '''
+    bList = []
+    for i in range(len(aList)):
+        if len(aList[i]) < 4:
+            bList.append(aList[i])
+    return bList
 
-##print lessThan4(["apple", "cat", "dog", "banana"])
+print lessThan4(["apple", "cat", "dog", "banana"])
 
 ##PROBLEM 6  (10 points possible)
 ##Write a recursive Python function, given a non-negative integer N,
@@ -208,18 +208,18 @@
 ##This function has to be recursive; you may not use loops!
 ##
 ##This function takes in one integer and returns one integer.
-##
-##def sumDigits(N):
-##    '''
-##    N: a non-negative integer
-##    '''
-##    # Your code here
-##    if N == 0:
-##        return 0
-##    else:
-##        return N%10 + sumDigits(N/10)
-##    
-##print sumDigits(1234)
+
+def sumDigits(N):
+    '''
+    N: a non-negative integer
+    '''
+    # Your code here
+    if N == 0:
+        return 0
+    else:
+        return N%10 + sumDigits(N/10)
+    
+print sumDigits(1234)
 
 ##Problem 7 (10/10 points)
 
@@ -268,7 +268,7 @@ def satisfiesF(L):
 
 run_satisfiesF(L, satisfiesF)
 
-For your own testing of satisfiesF, for example, the following test function f and test code:
+##For your own testing of satisfiesF, for example, the following test function f and test code:
 
 def f(s):
     return 'a' in s
@@ -282,26 +282,26 @@ Should print:
 2
 ['a', 'a']
 
-Paste your entire function satisfiesF, including the definition, 
-in the box below. After you define your function, make a function call to run_satisfiesF(L, satisfiesF). 
-Do not define f or run_satisfiesF. Do not leave any debugging print statements.
+##Paste your entire function satisfiesF, including the definition, 
+##in the box below. After you define your function, make a function call to run_satisfiesF(L, satisfiesF). 
+##Do not define f or run_satisfiesF. Do not leave any debugging print statements.
+##
+##For this question, you will not be able to see the test cases we run. 
+##This problem will test your ability to come up with your own test cases.
 
-For this question, you will not be able to see the test cases we run. 
-This problem will test your ability to come up with your own test cases.
-
-Solution 1:
+##Solution 1:
 def f(s):
     return 'a' in s
 
-def satisfiesF(L):
-    """
-    Assumes L is a list of strings
-    Assume function f is already defined for you and it maps a string to a Boolean
-    Mutates L such that it contains all of the strings, s, originally in L such
-            that f(s) returns True, and no other elements
-    Returns the length of L after mutation
-    """
-    # Your function implementation here
+##def satisfiesF(L):
+##    """
+##    Assumes L is a list of strings
+##    Assume function f is already defined for you and it maps a string to a Boolean
+##    Mutates L such that it contains all of the strings, s, originally in L such
+##            that f(s) returns True, and no other elements
+##    Returns the length of L after mutation
+##    """
+##    # Your function implementation here
 
     newL = []
     for i in L:
@@ -313,29 +313,26 @@ def satisfiesF(L):
     L[:] = newL	
 
     return len(L)
+##
+##L = ['a', 'b', 'a','a','c','a','p','w']
+L = ['a', 'b', 'a']
+##
+##print satisfiesF(L)
+##print L
 
-#L = ['a', 'b', 'a','a','c','a','p','w']
-#L = ['a', 'b', 'a']
-
-print satisfiesF(L)
-print L
-
-Solution 2:
+##Solution 2:
     
 def satisfiesF(L):
+    
+   rec_list=[]
 
-   count= len(L)
-   rec_list=list()
-
-   for i in range(0,count):
+   for i in range(0,len(L)):
      if(f(L[i])==False):
-        rec_list.append(L[i])   
-
-   if(rec_list):
-     for j in rec_list:
-      L.remove(j)
-
-
+        rec_list.append(L[i])
+        
+   for j in rec_list:
+       L.remove(j)
 
    return len(L)
-run_satisfiesF(L, satisfiesF)
+print satisfiesF(L)
+##run_satisfiesF(L, satisfiesF)
